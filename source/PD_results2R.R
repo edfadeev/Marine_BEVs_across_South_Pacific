cba1b9a9f5bbfe2fa12de759ab3da891d7fcf8ed
Eddie <dr.eduard.fadeev@gmail.com>
2025-09-26 10:57:09 +0200

    Mergin taxonomy of protins from blast and kaiju

--- source/PD_results2R.R
+++ source/PD_results2R.R
@@ -34,7 +34,10 @@ protein_metadata<- read.table("data/SO289_clust99_RC_PeptideGroups.txt", sep ="\
   summarize(Number.of.Peptides=n(), Number.of.PSMs=sum(Number.of.PSMs), Number.of.Unique.Peptide=sum(Unique.Peptides), Number.of.Razor.Peptide=sum(Razor.Peptides)) %>% 
   filter(#Number.of.Peptides>1, 
          Number.of.Unique.Peptide> 0) %>% # remove proteins with less than 2 peptides and without unique peptides
-  as.data.frame()
+  left_join(read.table("data/SO289-detected-proteins-calls.txt") %>%
+              mutate(Length=nchar(aa_sequence),gene_callers_id=as.character(gene_callers_id)) %>% 
+              select(gene_callers_id, contig, partial, Length, aa_sequence),
+            by="gene_callers_id") %>% as.data.frame()
 
 row.names(protein_metadata)<- protein_metadata$gene_callers_id
 
@@ -51,12 +54,6 @@ protein_abund<- read.table("data/SO289_clust99_RC_PeptideGroups.txt", sep ="\t",
   tibble::column_to_rownames("gene_callers_id")%>% 
   mutate(across(where(is.numeric), ~na_if(., 0)))
 
-
-#log2 transformation of protein abundances
-protein_abund.log2 <- log2(as.matrix(protein_abund))
-
-#median normalization of the data
-prot.dat.log2_norm = equalMedianNormalization(protein_abund.log2)
 
 ############################
 #Protein annotations
@@ -104,14 +101,18 @@ protein_annotations <- interpro_annotations_df %>%
 #import refseq taxonomy
 protein_taxonomy_refseq<- read.table("data/SO289-detected_proteins_kaiju_taxa_refseq.tsv", h=T, sep="\t") %>% 
   tidyr::separate(taxa, into = c("Domain","Phylum", "Order","Class", "Family","Genus", "Species"), sep = ";") %>% 
-  mutate(gene_callers_id=as.character(gene_callers_id)) %>% mutate_if(is.character, str_trim)
+  mutate(gene_callers_id=as.character(gene_callers_id)) %>% mutate_if(is.character, str_trim) %>% 
+  mutate(Order = case_when(Order=="NA" ~ paste0(Class, "_uncl"), TRUE~Order))
 
 #import nr taxonomy
 protein_taxonomy_nr<- read.table("data/SO289-detected_proteins_kaiju_taxa_nr.tsv", h=T, sep="\t") %>% 
   tidyr::separate(taxa, into = c("Domain","Phylum", "Order","Class", "Family","Genus", "Species"), sep = ";") %>% 
   mutate(Order=gsub(" ","",Order),Class=gsub(" ","",Class) ) %>% 
-  mutate(Order = case_when(Order=="NA" ~ paste0(Class, "_uncl"),
-                          TRUE~Order)) %>% 
+  mutate(Order = case_when(Order=="NA" ~ paste0(Class, "_uncl"), Order=="CandidatusPelagibacterales"~"Candidatus Pelagibacterales",
+                          TRUE~Order),
+         Family = case_when(Order=="Candidatus Pelagibacterales" ~ "Candidatus Pelagibacteraceae", 
+                            Genus %in% c("Candidatus Pseudothioglobus","Candidatus Thioglobus") ~ "Candidatus Pseudothioglobaceae",
+                            TRUE ~Family)) %>% 
   mutate_if(is.character, str_trim) %>% 
   mutate(gene_callers_id=as.character(gene_callers_id))
 
@@ -122,92 +123,58 @@ protein_taxonomy_kaiju<- protein_taxonomy_refseq %>% filter(NCBI_taxID==0) %>%
   rbind(protein_taxonomy_refseq %>% filter(NCBI_taxID!=0))
 
 #complement still missing values from blastp hits
-protein_taxonomy_blastp<- blastp_hits_df %>% 
-tidyr::separate(blastp_taxa, into = c("Domain","Phylum", "Order","Class", "Family","Genus", "Species"), sep = ",") %>% 
+protein_taxonomy_blastp<- blastp_hits_df %>% filter(!grepl("MULTISPECIES:", blastp_ann)) %>% 
+tidyr::separate(blastp_taxa, into = c("Domain","Kingdom", "Phylum", "Class", "Order", "Family","Genus"), sep = ",") %>% 
+  rename(Species=blastp_species) %>% 
   mutate(gene_callers_id=as.character(gene_callers_id)) %>% 
-  mutate_if(is.character, str_trim)
+  mutate_if(is.character, str_trim) %>% select(-Kingdom)
 
-protein_taxonomy<- protein_taxonomy_kaiju %>% filter(is.na(NCBI_taxID))%>% select(gene_callers_id) %>% 
+protein_taxonomy<- protein_taxonomy_kaiju %>% filter(NCBI_taxID==0)%>% select(gene_callers_id) %>% 
   left_join(protein_taxonomy_blastp %>% select(-starts_with("blastp")), by = c("gene_callers_id")) %>% 
-  rbind(protein_taxonomy_kaiju %>% filter(!is.na(NCBI_taxID)) %>% select(-c("NCBI_taxID")))
+  rbind(protein_taxonomy_kaiju %>% filter(NCBI_taxID!=0) %>% select(-c("NCBI_taxID")))
+
+
+protein_taxonomy<- protein_taxonomy %>% filter(grepl("_uncl", Order)) %>% select(gene_callers_id, Domain, Phylum, Class) %>% 
+  left_join(protein_taxonomy_blastp, by =c("gene_callers_id", "Domain","Phylum", "Class")) %>% 
+  select(c("gene_callers_id", "Domain","Phylum", "Order","Class", "Family","Genus", "Species")) %>% 
+  rbind(protein_taxonomy %>% filter(!grepl("_uncl", Order))) %>% 
+  mutate(Order=case_when(is.na(Order)~paste0(Class, "_uncl"), TRUE ~ Order))
 
 rm(protein_taxonomy_blastp, protein_taxonomy_kaiju, protein_taxonomy_nr, protein_taxonomy_refseq)
 rm(protease_annotation_df, blastp_hits_df, CAZYme_annotation_df, interpro_annotations_df)
+
 ############################
 #protein localization estimates
 ############################
 protein_localization <- read.table("data/SO289-detected_proteins_deeploc.txt", h=T, sep ="\t") %>% 
-  mutate(gene_callers_id = as.character(gene_callers_id)) %>% 
-  filter(gene_callers_id %in% row.names(prot.dat.log2_norm))
+  mutate(gene_callers_id = as.character(gene_callers_id))
 
 ############################
-#summarize number of proteins
+#Explore viral proteins in BEVs fraction
 ############################
-protein_abund_per_sample <- protein_abund %>%
+vir_gcids_taxa<- protein_taxonomy %>% 
+  filter(grepl("Viruses", Domain))%>% pull(gene_callers_id)
+
+vir_gcids_ann <-protein_annotations %>% 
+  filter(grepl("phage|virus|capsid|Tail sheath",Pfam_ann, ignore.case = TRUE)|
+           grepl("phage|virus|capsid|Tail sheath",InterPro_ann, ignore.case = TRUE)|
+           NCBIfam_acc %in% c("TIGR01554", "TIGR02126", "TIGR01543")) %>% pull(gene_callers_id)
+vir_gcids<- c(vir_gcids_taxa, vir_gcids_ann)
+
+#summarize how many viral proteins per sample
+viral_prot_per_sample <- protein_abund[vir_gcids,] %>%
   reshape2::melt(variable.name = "Sample_ID", value.name = "Abund") %>% 
   filter(Abund>0) %>% 
   left_join(samples_df, by = "Sample_ID") %>% 
   group_by(Region,Station_ID,Fraction) %>% 
-  summarize(N_prot=n()) 
+  summarize(N_prot=n()) %>% 
+  tidyr::spread(Fraction, N_prot)
 
-#plot
-protein_abund_per_sample %>% 
-  ggplot(aes(x=Station_ID, y=N_prot))+
-  geom_col()+
-  facet_wrap(Fraction~., scales = "free")+
-  xlab("Station ID")+ ylab("Number of proteins")+
-  theme_EF+
-  theme(axis.text.x=element_text(angle=90))
+#remove viral proteins
+protein_abund<- protein_abund[!row.names(protein_abund) %in% vir_gcids,]
 
+#log2 transformation of protein abundances
+protein_abund.log2 <- log2(as.matrix(protein_abund))
 
-############################
-#Check differences in protein composition between fraction
-############################
-prot.dat.MDS<- prot.dat.log2_norm
-prot.dat.MDS[is.na(prot.dat.MDS)]<- 0
-prot.dat.MDS<- prot.dat.MDS[!rowSums(prot.dat.MDS)==0,]
-
-# Running NMDS in vegan (metaMDS)
-prot_NMDS <-  metaMDS(t(prot.dat.MDS),
-                      distance = "euclidean",
-                      k = 2,
-                      maxit = 999, 
-                      trymax = 999,
-                      wascores = FALSE,
-                      autotransform = FALSE,
-                      tidy= "sites",
-                      na.rm = TRUE)
-
-# Perform K-means clustering with 4 clusters
-kmeans_result <- kmeans(t(prot.dat.MDS), centers=2, nstart = 25)
-
-#plot
-prot_NMDS.scores <- as.data.frame(scores(prot_NMDS)) %>% 
-  tibble::rownames_to_column(var ="Sample_ID") %>% 
-  left_join(samples_df, 
-            by = "Sample_ID", copy = TRUE) %>% 
-  mutate(Cluster=kmeans_result$cluster)
-
-prot_NMDS.scores %>% 
-  ggplot(aes(x=NMDS1, y=NMDS2, shape = Fraction, 
-             colour = as.factor(Cluster), label = Sample_ID))+
-  geom_point(size =5, colour = "black")+
-  geom_point(size =4)+
-  geom_text(nudge_y = -0.05, size =4)+
-  scale_color_manual(values = c("#009E73", "#F0E442", "#0072B2", 
-                                "#D55E00"))+
-  theme_bw()
-
-#test whether the differences between the runs are significant
-prot_distmat <- 
-  vegdist(t(prot.dat.MDS), method = "euclidean",na.rm = TRUE)
-
-df <- samples_df %>% 
-  select(Region,Fraction, Sample_ID) %>% 
-  mutate(Cluster=kmeans_result$cluster)
-
-adonis_all <- adonis2(prot_distmat ~ Fraction+Cluster, df,
-                      permutations=999)
-
-adonis_all
-
+#median normalization of the data
+prot.dat.log2_norm = equalMedianNormalization(protein_abund.log2)
